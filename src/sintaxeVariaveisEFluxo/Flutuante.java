f50336631d7657b00c84b4acf4511c45050219d8
Gabriel B <gabriel.moura2009@hotmail.com>
2021-07-16 10:56:11 -0300

    iniciando os estudos em java 8, aprendendo o básico

---
+++ src/sintaxeVariaveisEFluxo/Flutuante.java
@@ -0,0 +1,28 @@
+package sintaxeVariaveisEFluxo;
+
+/**
+ *
+ * @author gabri
+ */
+public class Flutuante {
+
+    public static void main(String[] args) {
+        double salario = 1125.50;
+        
+        // printf(salario);
+        
+        System.out.println("Meu salário  " + salario);
+        
+        double divisao = 16541 / 15;
+        
+        System.out.println("divisão = " + divisao);
+        
+        double outraDivisao = 5 / 2;
+        
+        System.out.println("outraDivisao " + outraDivisao);
+        
+        double outraDivisaoD = 5.0 / 2;
+        
+        System.out.println("outraDivisaoD " + outraDivisaoD);
+    }
+}
--- src/sintaxeVariaveisEFluxo/TestaVariaveis.java
+++ src/sintaxeVariaveisEFluxo/TestaVariaveis.java
@@ -4,8 +4,12 @@ package sintaxeVariaveisEFluxo;
  *
  * @author gabri
  */
-public class Variaveis {
+public class TestaVariaveis {
     public static void main(String[] args) {
         System.out.println("teste 2");
+        
+        int idade = 37;
+        
+        System.out.println("A idade do Paulo " + (idade * 3));
     }
 }
